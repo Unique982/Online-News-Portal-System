553797ce04acd417dc6d0cfba293651d2fae870c
Unique Neupane <133880428+Unique982@users.noreply.github.com>
2024-05-17 18:56:42 +0545

    Update sidebar.php
    Update code

--- user page/sidebar.php
+++ user page/sidebar.php
@@ -1,95 +1,88 @@
-
-
-    <style>
-        .serach-container{
-            display: flex;
-            background-color: #fefefe;
-            flex-wrap: flex;
-            justify-content: flex-end;
-            align-items: center; 
-            height: 50vh;
-      
-        }
-        .serach-box{
-            padding: 10px;
-            background-color: #eee;
-            border-radius: 5px;
-           
-            width: 300px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.5);
-            box-sizing: border-box;
-        }
-        .serach-box input[type="text"]{
-            width: 100%;
-            padding: 10px;
-            border-radius: 2px;
-            box-sizing: border-box;
-            margin-bottom: 7px;
-            
-        }
-        .serach-box input[type="submit"]{
-            width: 100%;
-            padding: 10px;
-            background-color: #0fffff;
-            color: #000;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-
-        }
+<style>
+    .recent-post{
+        margin-top: 20px;
+        background-color: #fefe;  
+        width: 100%;
+       
+    }
+    .recent-content{
+        padding: 10px;
         
-    </style>
+    }
+    
+    .recent  .tit {
+        color: #333;
+        text-decoration: none;
+        display: block;
+    }
+    .recent_img {
+    display: flex;
+    
+    align-items: center;
+    padding: 10px;  
+}
+.recent_img img {
+    margin-right: 10px;
+    border: 4px solid;
+}
+ h5 a {
+    text-decoration: none;
+    color: #333;
+    font-size: 18px;
+   
+}
 
-<div class="sidebar">
-<div class="serach-box">
-    <h4>Search</h4>
-    <form action="search.php" method="GET">
-        <input type="text" name="search_term" placeholder="Serach">
-        <input type="submit" value = "Search" name="Search">
-    </form>
+</style>
+<div class="recent">
+        
+        <h2 class="title">Search</h2>
+<form action="search.php" method="GET" class="search-post">
+
+<div class="input-box">
+    <input type="text" name="search" placeholder="Search...">
+    <button type="submit" class="btn">Search</button>
 </div>
-<div class="recent-post-container">
-    <h4>Recent Post</h4>
-    <?php
+</form>
+
+<div class="recent-post">
     
-     include "database.php";
-     $limit =4;
-     $sql = "SELECT news_post.post_id, news_post.post_title, news_post.category,news_post.date, post_category.category_name,news_post.category ,news_post.post_Uploadimg FROM news_post 
-     LEFT JOIN post_category ON news_post.category =post_category.category_id
-      ORDER BY news_post.post_id DESC LIMIT {$limit}";
-     $result = mysqli_query($conn, $sql) or die("Query Failed: Recenr Post");
- 
-     if (mysqli_num_rows($result) > 0) {
-         while ($row = mysqli_fetch_assoc($result)) {
- 
- 
-    
-    
-    ?>
-    <div class="recent-post">
-    <a href="single.php?id=<?php echo $row['post_id']; ?>"> 
-    <img src="../admin/Post UploadImg/<?php echo $row['post_Uploadimg']; ?>" alt="" width="100px" height="100px">
-                    </a>
-        <div class="post-content">
-            <h5>
-        <a href="single.php?id=<?php echo $row['post_id'] ?>" class="post-title"><?php echo $row['post_title'] ?></a></h5>
-            <span>
-                        <i class="fa-solid fa-tag"></i><a href='Category.php?category_id=<?php echo $row['category']; ?>'><?php echo $row['category_name']; ?></a>
-                    </span>
-                   
-                        <i class="fa-solid fa-calendar-days"></i><a href="#time"><?php echo $row['date']; ?></a>
-                    </span>
-                 <div>
-                    <a href='single.php?id=<?php echo $row['post_id']; ?>'>Read More</a>
-                    </div>
-                  
-        </div>
-    </div>
-</div>
+    <div class="recent-content">
+        
+<h2 class="title">Recent Post</h2>
 <?php
-         }} else{
-            echo "not Found";
-         }
+    include "../database/database.php";
+  $limit = 4;
+    $sql2 = "SELECT news_post.post_id, news_post.post_title, news_post.category,news_post.date, post_category.category_name,news_post.category ,news_post.post_Uploadimg FROM news_post 
+    LEFT JOIN post_category ON news_post.category =post_category.category_id
+     ORDER BY news_post.post_id DESC LIMIT {$limit}";
+    $result = mysqli_query($conn, $sql2) or die("Query Failed: Recenr Post");
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+
+
+   
+   
+   ?>
+   
+ <div class="recent_img">
+ <a href="single.php?id=<?php echo $row['post_id']; ?>"> 
+<img src="../admin/Post UploadImg/<?php echo $row['post_Uploadimg']; ?>" alt="" width="100px" height="80px">
+                </a>
+  
+   <h5><a href="single.php?id=<?php echo $row['post_id'] ?>" class="post-title"><?php echo $row['post_title'] ?></a></h5>
+<hr>
+</div>  
+
+<?php
+     }} else{
+        echo "not Found";
+     }
 
 ?>
+</div></div>
+
+
+
 </div>
+    </div>
